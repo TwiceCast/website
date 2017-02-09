321920efaf53ba9d7d6097d65519cdede7590371
Elliott Zwertvaegher <elliott.zwertvaegher@epitech.eu>
2017-02-09 21:57:51 +0900

    Displaying ProfilDetail based on API

--- app/pages/profil/profilDetail.component.ts
+++ app/pages/profil/profilDetail.component.ts
@@ -1,5 +1,10 @@
 // Imports
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+import { User } from '../../models/user.model';
+import { APILinker } from '../../services/APILinker.service';
+import { Logger } from '../../services/Logger.service';
 
 @Component({
   selector: 'component-profilDetail',
@@ -7,4 +12,24 @@ import { Component } from '@angular/core';
 })
 
 // Component class
-export class ProfilDetailComponent {}
+export class ProfilDetailComponent implements OnInit, OnDestroy {
+    private id: number;
+    private sub: any;
+    
+    private user: User;
+    
+    constructor(private route: ActivatedRoute, private api:APILinker, private logg:Logger) {
+    }
+    
+    ngOnInit() {
+        this.sub = this.route.params.subscribe(params => {
+            this.id = +params['id'];
+            this.api.getUser(this.id).then(response => this.user = response);
+        });
+    }
+    
+    ngOnDestroy() {
+        this.sub.unsubscribe();
+    }
+
+}
--- app/services/APILinker.service.ts
+++ app/services/APILinker.service.ts
@@ -20,6 +20,15 @@ export class APILinker {
                         .catch(this.handleError);
     }
     
+    getUser(id: number): Promise<User> {
+        let headers = new Headers({ 'Content-Type': 'application/json' });
+        let options = new RequestOptions({ headers: headers });
+        return this.http.get(this.API_URL + "users/" + id, options)
+                        .toPromise()
+                        .then(response => response.json() as User)
+                        .catch(this.handleError);
+    }
+    
     private handleError(error: any): Promise<any> {
     console.error('An error occurred', error); // for demo purposes only
     return Promise.reject(error.message || error);
