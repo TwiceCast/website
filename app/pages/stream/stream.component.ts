1f05c002c8fb0ba3939da11709840c23145b6ac5
Elliott Zwertvaegher <elliott.zwertvaegher@epitech.eu>
2017-07-11 20:07:32 +0900

    Better server status display

--- app/pages/stream/stream.component.ts
+++ app/pages/stream/stream.component.ts
@@ -39,6 +39,7 @@ export class StreamComponent implements OnInit, OnDestroy {
     
 
     private mysock: any;
+	private displayErrorConnect: boolean;
     
     logCode()
     {
@@ -59,11 +60,20 @@ export class StreamComponent implements OnInit, OnDestroy {
         this.chatMessages = [];
         this.mysock = io('http://localhost:3006');
         
+		this.displayErrorConnect = true;
+		
+		var connecting_message = new ChatMessage();
+		connecting_message.id = -1;
+		connecting_message.author = "";
+		connecting_message.message = "Joining chat...";
+		this.chatMessages.push(connecting_message);
+		
         this.mysock.on('connect', function(data:any){
+			this.displayErrorConnect = true;
 			var logging_in_message = new ChatMessage();
 			logging_in_message.id = -1;
 			logging_in_message.author = "";
-			logging_in_message.message = "Joining chat...";
+			logging_in_message.message = "Logging in...";
 			this.chatMessages.push(logging_in_message);
             this.mysock.emit('auth', {'username':'test@test.com', 'password':'loltest', 'room':'stream1'});
         }.bind(this));
@@ -84,7 +94,26 @@ export class StreamComponent implements OnInit, OnDestroy {
         
         this.mysock.on('cerror', function(data:any){
             console.error('CHAT ERROR (' + data.code + '): ' + data.message);
-        }.bind(this));        
+        }.bind(this));
+		
+		this.mysock.on('connect_error', function(error:any){
+			if (this.displayErrorConnect == true) {
+				var error_connect_message = new ChatMessage();
+				error_connect_message.id = -1;
+				error_connect_message.author = "";
+				error_connect_message.message = "Couldn't connect ! Waiting to reconnect...";
+				this.chatMessages.push(error_connect_message);
+				this.displayErrorConnect = false;
+			}
+		}.bind(this));
+		
+		this.mysock.on('disconnect', function(reason:any){
+			var disconnect_message = new ChatMessage();
+			disconnect_message.id = -1;
+			disconnect_message.author = "";
+			disconnect_message.message = "You got disconnected !";
+			this.chatMessages.push(disconnect_message);
+		}.bind(this));
     }
     
     ngOnDestroy() {
