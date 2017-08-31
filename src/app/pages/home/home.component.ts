7bb9d5cd4c3e54c3cccd5eb68bcf6723aff3c041
Elliott Zwertvaegher <elliott.zwertvaegher@epitech.eu>
2017-08-31 10:58:43 +0900

    Popular streams linked (partialy) to API

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -16,7 +16,8 @@ import { APILinker } from '../../services/APILinker.service';
 export class HomeComponent {
 
     private all_stream: Stream[];
-    public random_streams: Stream[] = null;
+    public popular_streams: Stream[];
+    public random_streams: Stream[];
 
     constructor(private logg:Logger, public linker:APILinker, public sm:SessionManager) {}
 
@@ -25,11 +26,12 @@ export class HomeComponent {
             .then((response) => {
                 this.all_stream = response;
                 this.SelectRandomStreams();
+                this.SelectPopularStreams();
             })
             .catch(() => {this.logg.error('(home component) couldn\'t retrieve streams');});
     }
     
-    SelectRandomStreams(): void {
+    private SelectRandomStreams(): void {
         if (this.all_stream.length < 6 && this.all_stream.length > 0) {
             this.random_streams = this.all_stream;
         } else if (this.all_stream.length > 0) {
@@ -37,6 +39,19 @@ export class HomeComponent {
                 let index = Math.floor(Math.random() * (this.all_stream.length));
                 if (this.random_streams.find(x => x == this.all_stream[index]) == null) {
                     this.random_streams.push(this.all_stream[index]);
+                }
+            }
+        }
+    }
+    
+    private SelectPopularStreams(): void {
+        if (this.all_stream.length < 6 && this.all_stream.length > 0) {
+            this.popular_streams = this.all_stream;
+        } else if (this.all_stream.length > 0) {
+            for (let i = 0; i < 6; i++) {
+                let index = Math.floor(Math.random() * (this.all_stream.length));
+                if (this.popular_streams.find(x => x == this.all_stream[index]) == null) {
+                    this.popular_streams.push(this.all_stream[index]);
                 }
             }
         }
