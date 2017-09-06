6a28b684248ed95f7a900f9f40c3047b91ab0760
Nicolas Ribeiro Teixeira <nicolas.ribeiro-teixeira@epitech.eu>
2017-09-07 00:07:32 +0200

    Change password design ok

--- src/app/pages/profil/profilDetail.component.ts
+++ src/app/pages/profil/profilDetail.component.ts
@@ -5,6 +5,7 @@ import { ActivatedRoute } from '@angular/router';
 import { User } from '../../models/user.model';
 import { APILinker } from '../../services/APILinker.service';
 import { Logger } from '../../services/Logger.service';
+import { SessionManager } from '../../services/SessionManager.service'
 
 @Component({
   selector: 'component-profilDetail',
@@ -15,12 +16,12 @@ import { Logger } from '../../services/Logger.service';
 // Component class
 export class ProfilDetailComponent implements OnInit, OnDestroy {
     public id: number;
-    public isCurrentUser: boolean;
+    public isCurrentUser: boolean = false;
     private sub: any;
     
     user: User;
     
-    constructor(private route: ActivatedRoute, private api:APILinker, private logg:Logger) {
+    constructor(private route: ActivatedRoute, private api:APILinker, private logg:Logger, private sm:SessionManager) {
     }
     
     ngOnInit() {
@@ -28,10 +29,13 @@ export class ProfilDetailComponent implements OnInit, OnDestroy {
             this.id = params['id'];
             this.api.getUser(this.id).then(response => {
                 this.user = response;
-                console.log(this.user);
+                this.sm.RetrieveUser()
+                if (this.sm.getUser().id == this.id)
+                {
+                    this.isCurrentUser = true;
+                }
             });
-        });
-        this.isCurrentUser = true;
+        });            
     }
     
     ngOnDestroy() {
